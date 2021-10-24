19d4d4d954d8d4477012191ceafb4c5db193cee5
Dennis Neumann <dennis2598@gmx.net>
2021-10-25 00:26:22 +0200

    Initial VuePress project

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    lang: 'en-US',
+    title: 'FeuerMania',
+    description: 'This is the official FeuerMania website.',
+  
+    themeConfig: {
+      logo: 'https://vuejs.org/images/logo.png',
+    },
+  }
